50bbcb176fe353cde11af25b595d5b58a06b5e86
misaki.janet <misaki.janet@gmail.com>
2023-12-25 23:12:24 +0900

    [Add] HShop Project

---
+++ src/main/java/com/vti/Hshop/HshopApplication.java
@@ -0,0 +1,13 @@
+package com.vti.Hshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/Hshop/HshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.Hshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
